d5bdd739d3ab9f90ef9203a1e8df40ee78d08cf8
AntonLiu <haoyuliuse@gmail.com>
2023-08-07 16:21:49 +0800

    数组生成类扩展为Helper类，并增加打印数组函数

--- test/array_helper.cpp
+++ test/array_helper.cpp
@@ -2,17 +2,18 @@
 // Created by Anton Liu on 2023/8/7.
 //
 
-#include "array_generator.h"
+#include "array_helper.h"
+#include <random>
 using std::vector, std::default_random_engine, std::uniform_real_distribution, std::uniform_int_distribution;
 
-vector<int> ArrayGenerator::generateOrderedIntArray(size_t n) {
+vector<int> ArrayHelper::generateOrderedIntArray(size_t n) {
     vector<int> vec(n);
     for(int i = 0; i < n; i++)
         vec[i] = i;
     return vec;
 }
 
-vector<int> ArrayGenerator::generateRandomIntArray(size_t n, int lowerBound, int upperBound) {
+vector<int> ArrayHelper::generateRandomIntArray(size_t n, int lowerBound, int upperBound) {
     vector<int> vec(n);
     default_random_engine e;
     uniform_int_distribution<int> u(lowerBound, upperBound);
@@ -22,14 +23,14 @@ vector<int> ArrayGenerator::generateRandomIntArray(size_t n, int lowerBound, int
 }
 
 
-vector<double> ArrayGenerator::generateOrderedFloatArray(size_t n) {
+vector<double> ArrayHelper::generateOrderedFloatArray(size_t n) {
     vector<double> vec(n);
     for(size_t i = 0; i < n; i++)
         vec[i] = static_cast<double>(i);
     return vec;
 }
 
-vector<double> ArrayGenerator::generateRandomFloatArray(size_t n, double lowerBound, double upperBound) {
+vector<double> ArrayHelper::generateRandomFloatArray(size_t n, double lowerBound, double upperBound) {
     vector<double> vec(n);
     default_random_engine e;
     uniform_real_distribution<double> u(lowerBound, upperBound);
---
+++ test/array_helper.h
@@ -0,0 +1,37 @@
+//
+// Created by Anton Liu on 2023/8/7.
+//
+
+#ifndef ALGORITHM_TEMPLATE_CPP_ARRAY_HELPER_H
+#define ALGORITHM_TEMPLATE_CPP_ARRAY_HELPER_H
+
+#include <vector>
+#include <iostream>
+
+class ArrayHelper {
+public:
+    ArrayHelper() = delete;
+
+    // 生成数组
+    static std::vector<int> generateOrderedIntArray(size_t n);
+    static std::vector<int> generateRandomIntArray(size_t n, int lowerBound, int upperBound);  // [lowerBound, upperBound]
+    static std::vector<double> generateOrderedFloatArray(size_t n);
+    static std::vector<double> generateRandomFloatArray(size_t n, double lowerBound, double upperBound);  // [lowerBound, upperBound]
+
+    // 打印数组
+    template <typename T> static void printArray(const std::vector<T> &arr);
+};
+
+template<typename T>
+void ArrayHelper::printArray(const std::vector<T> &arr) {
+    if (arr.size() == 0)
+        return;
+    std::cout << "(1行10个元素)\n " << arr[0];
+    for (int i = 1; i < arr.size(); i++) {
+        if (i % 10 == 0) std::cout << std::endl;
+        std::cout << ' ' << arr[i];
+    }
+    std::cout << std::endl;
+}
+
+#endif //ALGORITHM_TEMPLATE_CPP_ARRAY_HELPER_H
