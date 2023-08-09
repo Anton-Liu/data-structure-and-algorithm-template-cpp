466f4675d472547dd026ea6491bf3eeaa2a8fbd2
AntonLiu <haoyuliuse@gmail.com>
2023-08-09 15:12:14 +0800

    增加数组栈类

---
+++ stack/array_stack.h
@@ -0,0 +1,61 @@
+#ifndef ALGORITHM_TEMPLATE_CPP_ARRAY_STACK_H
+#define ALGORITHM_TEMPLATE_CPP_ARRAY_STACK_H
+
+#include "../array/lazy_dynamic_array.h"
+#include <memory>
+#include <iostream>
+
+template <typename T>
+class ArrayStack;
+
+template <typename T>
+std::ostream &operator<<(std::ostream &, const ArrayStack<T> &);
+
+template <typename T>
+class ArrayStack {
+    friend std::ostream &operator<<<T>(std::ostream &os, const ArrayStack<T> &arrayStack);
+public:
+    ArrayStack():
+            arr(new LasyDynamicArray<T>()) { }
+    ArrayStack(int capacity):
+            arr(new LasyDynamicArray<T>(capacity)) { }
+
+    bool isEmpty() const { return arr -> isEmpty(); }
+    int getSize() const { return arr -> getSize(); }
+    int getCapacity() const { return arr -> getCapacity(); }
+    T peek() const { return arr -> getLast(); }
+
+    void push(const T &e) { arr -> addLast(e); };
+    void pop() { arr -> removeLast(); }
+private:
+    std::unique_ptr<LasyDynamicArray<T>> arr;
+};
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const ArrayStack<T> &arrayStack) {
+    int size = arrayStack.arr -> getSize();
+    // 自适应边框
+    os << "-----------------";
+    for (int i = 0; i < size; i++)
+        os << "---";
+    os << '\n';
+
+    // 数组信息
+    os << "栈的长度：" << size << "，"
+       << "栈的容量：" << arrayStack.getCapacity() << '\n'
+       << "栈的内容：" << "stack[";
+    for (int i = 0; i < size; i++) {
+        os << (*arrayStack.arr)[i];
+        if (i != size - 1)
+            os << ", ";
+    }
+    os << "] top\n";
+
+    // 自适应边框
+    os << "-----------------";
+    for (int i = 0; i < size; i++)
+        os << "---";
+    return os;
+}
+
+#endif //ALGORITHM_TEMPLATE_CPP_ARRAY_STACK_H
