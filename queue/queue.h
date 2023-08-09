dd349cf8f045481c998582669ee841a79847727c
AntonLiu <haoyuliuse@gmail.com>
2023-08-09 20:19:27 +0800

    完善接口类

--- queue/queue.h
+++ queue/queue.h
@@ -3,12 +3,14 @@
 
 template <typename T>
 class Queue {
+public:
     virtual bool isEmpty() const = 0;
     virtual int getSize() const = 0;
     virtual int getCapacity() const = 0;
     virtual T getFront() const = 0;
     virtual void enqueue(const T &e) = 0;
     virtual void dequeue() = 0;
+    virtual ~Queue() { };
 };
 
 #endif //ALGORITHM_TEMPLATE_CPP_QUEUE_H
