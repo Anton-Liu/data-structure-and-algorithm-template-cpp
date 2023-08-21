758c4668a4cedbac37cadd86ab7177a5c52263da
AntonLiu <haoyuliuse@gmail.com>
2023-08-21 21:01:14 +0800

    增加指定范围的排序接口

--- sort/insertion_sort.h
+++ sort/insertion_sort.h
@@ -8,6 +8,7 @@ class InsertionSort {
 public:
     InsertionSort() = delete;  // 删除默认构造函数，该类不需要构造对象
     static void sort(std::vector<T> &arr);  // 排序(默认升序)
+    static void sort(std::vector<T> &arr, int l, int r);
 };
 
 template<typename T>
@@ -16,6 +17,22 @@ void InsertionSort<T>::sort(std::vector<T> &arr) {
         T e = arr[i];
         int j;
         for (j = i - 1; j >= 0 && arr[j] > e; j--)
+            arr[j + 1] = arr[j];
+        arr[j + 1] = e;
+    }
+}
+
+template<typename T>
+void InsertionSort<T>::sort(std::vector<T> &arr, int l, int r) {
+    if (l >= r)
+        return;
+    if (r >= arr.size())
+        throw std::runtime_error("索引超过数组范围！");
+
+    for (int i = l + 1; i <= r; i++) {
+        T e = arr[i];
+        int j;
+        for (j = i - 1; j >= l && arr[j] > e; j--)
             arr[j + 1] = arr[j];
         arr[j + 1] = e;
     }
