b06be515efe6c83b01f4ea5315a7e9be75b64148
AntonLiu <haoyuliuse@gmail.com>
2023-08-23 20:17:11 +0800

    增加upper和ceil

--- search/binary_search.h
+++ search/binary_search.h
@@ -13,6 +13,18 @@ public:
     // 另一种循环不变量的实现方式
     template <typename T>
     static int search2(const std::vector<T> &data, const T &target);  // 二分查找值为target的元素对应下标，不存在返回-1
+
+    // upper，返回大于target值的最小值的索引
+    // 若target值大于等于最后一个元素，则upper结果返回最后一个元素之后的索引(越界)
+    // 因此结果的范围是：[0, data.size()]
+    template <typename T>
+    static int upper(const std::vector<T> &data, const T &target);
+
+    // ceil
+    // 1 数组中存在等于target的元素，返回最大索引
+    // 2 数组中不存在等于target元素，返回upper
+    template <typename T>
+    static int ceil(const std::vector<T> &data, const T &target);
 };
 
 template<typename T>
@@ -48,5 +60,31 @@ int BinarySearch::search2(const std::vector<T> &data, const T &target) {
     }
     return -1;
 }
+
+
+template<typename T>
+int BinarySearch::upper(const std::vector<T> &data, const T &target) {
+    int l = 0;
+    int r = data.size();
+
+    // 在data[l, r]中寻找解(必定有解)
+    while (l < r) {
+        int mid = l + (r - l) / 2;
+        if (data[mid] <= target)
+            l = mid + 1;
+        else  // data[mid] > target
+            r = mid;  // data[r]本身可能就是答案
+    }
+    return l;  // l和r必定相等，也可以返回r
+}
+
+template<typename T>
+int BinarySearch::ceil(const std::vector<T> &data, const T &target) {
+    int u = upper(data, target);
+    if (u - 1 >= 0 && data[u - 1] == target)
+        return u - 1;
+    return u;
+}
+
 
 #endif //ALGORITHM_TEMPLATE_CPP_BINARY_SEARCH_H
