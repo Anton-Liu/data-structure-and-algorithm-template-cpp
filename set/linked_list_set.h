bace90f5b7a2e45082467a45779abf4741b00c98
AntonLiu <haoyuliuse@gmail.com>
2023-09-08 15:55:45 +0800

    增加输出函数

--- set/linked_list_set.h
+++ set/linked_list_set.h
@@ -1,11 +1,18 @@
 #ifndef ALGORITHM_TEMPLATE_CPP_LINKED_LIST_SET_H
 #define ALGORITHM_TEMPLATE_CPP_LINKED_LIST_SET_H
 
+#include <iostream>
 #include "set.h"
 #include "../linked_list/singly_linked_list.h"
 
+template <typename T> class LinkedListSet;
+
+template <typename T>
+std::ostream &operator<<(std::ostream &, const LinkedListSet<T> &);
+
 template <typename T>
 class LinkedListSet : public Set<T> {
+    friend std::ostream &operator<<<T>(std::ostream &os, const LinkedListSet<T> &rhs);
 public:
     LinkedListSet():
             bst(new SinglyLinkedList<T>()) { }
@@ -41,6 +48,36 @@ LinkedListSet<T> &LinkedListSet<T>::operator=(const LinkedListSet<T> &rhs) {
     return *this;
 }
 
+template<typename T>
+std::ostream &operator<<(std::ostream &os, const LinkedListSet<T> &rhs) {
+    int size = rhs.getSize();
 
+    // 自适应边框
+    os << "--------------------------";
+    for (int i = 0; i < size; i++)
+        os << "----";
+    os << '\n';
+
+    // 链表的信息
+    os << "Set的大小：" << size << "\n"
+       << "Set的内容：\n";
+
+    os << "{";
+    auto tmp = *rhs.bst;
+    for (int i = 0; i < size; i++) {
+        if (i != size - 1)
+            os << tmp.getFirst() << ", ";
+        else
+            os << tmp.getFirst();
+        tmp.removeFirst();
+    }
+    os << "}\n";
+
+    // 自适应边框
+    os << "--------------------------";
+    for (int i = 0; i < size; i++)
+        os << "----";
+    return os;
+}
 
 #endif //ALGORITHM_TEMPLATE_CPP_LINKED_LIST_SET_H
