b3dfe0a7f052780b5593905b2e7bfe6fe9dcdcfb
AntonLiu <haoyuliuse@gmail.com>
2023-09-23 10:36:17 +0800

    修改细节

--- heap/max_heap.h
+++ heap/max_heap.h
@@ -16,7 +16,7 @@ public:
     MaxHeap():
         data(new std::vector<T>()) { };
     explicit MaxHeap(int capacity):
-        data(new std::vector<T>(10)) { };
+        data(new std::vector<T>(capacity)) { };
     explicit MaxHeap(const std::vector<T> &arr);  // heapify
     MaxHeap(const MaxHeap<T> &rhs);
     MaxHeap &operator=(const MaxHeap<T> &rhs);
