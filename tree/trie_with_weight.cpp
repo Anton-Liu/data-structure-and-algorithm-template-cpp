e9ddb3c1cd20c78edecfe5e875cb9b6a49221be7
AntonLiu <haoyuliuse@gmail.com>
2023-10-04 14:12:22 +0800

    增加单词带权重的trie实现

---
+++ tree/trie_with_weight.cpp
@@ -0,0 +1,87 @@
+#include "trie_with_weight.h"
+
+void TrieWithWeight::add(const std::string &word, int weight) {
+    auto cur = root;
+    for (const auto &c : word) {
+        if (cur -> next -> find(c) == cur -> next -> end())
+            cur -> next -> insert({c, new Node()});
+        cur = cur -> next -> at(c);
+    }
+
+    if (!cur -> isWord) {  // 单词可能已经存在！
+        cur -> isWord = true;
+        size++;
+    }
+    // 若单词不存在，设置权重
+    // 若单词存在，更新权重
+    cur -> weight = weight;
+}
+
+void TrieWithWeight::setWeight(const std::string &word, int weight) {
+    auto cur = root;
+    for (const auto &c : word) {
+        if (cur -> next -> find(c) == cur -> next -> end())
+            throw std::runtime_error("单词不存在！");
+        cur = cur -> next -> at(c);
+    }
+    if (!cur -> isWord)
+        throw std::runtime_error("单词不存在！");
+    cur -> weight = weight;
+}
+
+bool TrieWithWeight::contains(const std::string &word) const {
+    auto cur = root;
+    for (const auto &c : word) {
+        if (cur -> next -> find(c) == cur -> next -> end())
+            return false;
+        cur = cur -> next -> at(c);
+    }
+
+    return cur -> isWord;
+}
+
+bool TrieWithWeight::isPrefix(const std::string &prefix) const {
+    auto cur = root;
+    for (const auto &c : prefix) {
+        if (cur -> next -> find(c) == cur -> next -> end())
+            return false;
+        cur = cur -> next -> at(c);
+    }
+
+    return true;
+}
+
+bool TrieWithWeight::contains(const std::string &word, char wildcard) const {
+    return match(root, word, wildcard, 0);
+}
+
+bool TrieWithWeight::match(const TrieWithWeight::Node* node, const std::string &word, char wildcard, int idx) const {
+    if (idx == word.size())
+        return node -> isWord;
+
+    if (word[idx] != wildcard) {
+        if (node -> next -> find(word[idx]) == node -> next -> end())
+            return false;
+        return match(node -> next -> at(word[idx]), word, wildcard, idx + 1);
+    }
+    else {
+        for (auto it = node -> next -> begin(); it != node -> next -> end(); ++it)
+            if (match(it -> second, word, wildcard, idx + 1))
+                return true;
+        return false;
+    }
+}
+
+int TrieWithWeight::getWeight(const std::string &word) const {
+    auto cur = root;
+    for (const auto &c : word) {
+        if (cur -> next -> find(c) == cur -> next -> end())
+            return false;
+        cur = cur -> next -> at(c);
+    }
+
+    if (!cur -> isWord)
+        throw std::runtime_error("单词不存在！");
+
+    return cur -> weight;
+}
---
+++ tree/trie_with_weight.h
@@ -0,0 +1,47 @@
+#ifndef ALGORITHM_TEMPLATE_CPP_TRIE_WITH_WEIGHT_H
+#define ALGORITHM_TEMPLATE_CPP_TRIE_WITH_WEIGHT_H
+
+#include <map>
+#include <string>
+
+class TrieWithWeight {
+    // friend std::ostream &operator<<(std::ostream &, const Trie &);
+public:
+    TrieWithWeight():
+        root(new Node()), size(0) { }
+
+    int getSize() const { return size; }  // 获取当前trie中含有的单词数量
+    bool isEmpty() const { return size == 0; }  // 判断trie中是否存在单词
+    bool isPrefix(const std::string &prefix) const;  // 查询Trie中是否含有单词以prefix为前缀
+    bool contains(const std::string &word) const;  // 查询Trie中是否含有单词word
+    bool contains(const std::string &word, char wildcard) const;  // 查询Trie中是否含有单词(允许使用word通配符)
+    int getWeight(const std::string &word) const;  // 查询Trie中某个单词的权重
+    void setWeight(const std::string &word, int weight);  // 更新Trie中单词word的权重
+    void add(const std::string &word, int weight);  // 向Trie中添加单词word(需要给出权重)
+
+    ~TrieWithWeight() { delete root; }
+private:
+    class Node {
+    public:
+        bool isWord;  // 访问到当前Node时，是否能确定一个单词
+        int weight;  // 记录单词的权重
+        std::map<char, Node*> *next;
+        Node():
+            isWord(false), weight(0), next(new std::map<char, Node*>()) { }
+        explicit Node(bool isWord, int weight):
+            isWord(isWord), weight(weight), next(new std::map<char, Node*>()) { }
+        ~Node() { delete next; }
+    };
+
+    Node *root;  // 根结点
+    int size;  // 记录当前Trie中单词数量
+
+    // 递归函数，完成支持通配符的查询
+    // 如果存在字符串与word匹配，则返回true；否则，返回false
+    // word中可能包含一些通配符，每个通配符都可以表示任何一个字母。
+    bool match(const Node *node, const std::string &word, char wildcard, int idx) const;
+};
+
+// std::ostream &operator<<(std::ostream &, const Trie &);
+
+#endif //ALGORITHM_TEMPLATE_CPP_TRIE_WITH_WEIGHT_H
