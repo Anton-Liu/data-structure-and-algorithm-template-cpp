9cdcfd6ef923128d817e20bf137a4e69ace2f373
AntonLiu <haoyuliuse@gmail.com>
2023-10-05 14:41:54 +0800

    去掉多余注释

--- tree/union_find_quick_find.h
+++ tree/union_find_quick_find.h
@@ -25,15 +25,15 @@ public:
     }
 
     int getSize() const override { return id -> size(); };
-    bool isConnected(int p, int q) override  // 查看元素p和q是否同属于一个集合，时间复杂度：O(1)
+    bool isConnected(int p, int q) override  // 查看元素p和q是否同属于一个集合
         { return find(p) == find(q); }
-    void unionElements(int p, int q) override;  // 合并元素p和q所属的集合，时间复杂度：O(n)
+    void unionElements(int p, int q) override;  // 合并元素p和q所属的集合
 
     ~UnionFindQuickFind() { delete id; }
 private:
     std::vector<int> *id;
 
-    int find(int p);   // 查找元素p所对应的集合编号，时间复杂度：O(1)
+    int find(int p);   // 查找元素p所对应的集合编号
 };
 
 std::ostream &operator<<(std::ostream &, const UnionFindQuickFind &);
