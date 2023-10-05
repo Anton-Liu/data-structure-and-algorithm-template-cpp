4443047c8ffffbd0a03180b81c62ee9857d77a84
AntonLiu <haoyuliuse@gmail.com>
2023-10-05 14:31:57 +0800

    新增QuickFind并查集的实现

---
+++ tree/union_find_quick_find.cpp
@@ -0,0 +1,52 @@
+#include "union_find_quick_find.h"
+
+int UnionFindQuickFind::find(int p) {
+    if (p < 0 || p > id -> size())
+        throw std::runtime_error("索引非法！");
+    return (*id)[p];
+}
+
+void UnionFindQuickFind::unionElements(int p, int q) {
+    auto pID = find(p);
+    auto qID = find(q);
+
+    if (pID == qID)
+        return;
+
+    for (auto &e : *id)
+        if (e == pID)
+            e = qID;
+}
+
+std::ostream &operator<<(std::ostream &os, const UnionFindQuickFind &uf) {
+    // 自适应边框
+    int size = uf.getSize();
+    os << "-----------------";
+    for (int i = 0; i < size; i++)
+        os << "---";
+    os << '\n';
+
+    // 数组信息
+    os << "并查集大小：" << size << "\n"
+       << "并查集数组表示：\n" << "index[";
+    for (int i = 0 ; i < size; i++) {
+        os << i;
+        if (i != size - 1)
+            os << ", ";
+    }
+    os << "]\n";
+
+    os << "   id[";
+    for (int i = 0 ; i < size; i++) {
+        os << (*uf.id)[i];
+        if (i != size - 1)
+            os << ", ";
+    }
+    os << "]\n";
+
+    // 自适应边框
+    os << "-----------------";
+    for (int i = 0; i < size; i++)
+        os << "---";
+    return os;
+}
---
+++ tree/union_find_quick_find.h
@@ -0,0 +1,40 @@
+#ifndef ALGORITHM_TEMPLATE_CPP_UNION_FIND_QUICK_FIND_H
+#define ALGORITHM_TEMPLATE_CPP_UNION_FIND_QUICK_FIND_H
+
+#include "union_find.h"
+#include <vector>
+#include <iostream>
+
+/**
+ * 最简单的并查集Quick Find
+ * 特点：
+ *      查询很快，合并慢
+ * 各操作时间复杂度：
+ *      isConnected: O(1)
+ *      find: O(1)
+ *      unionElements: O(n)
+ */
+
+class UnionFindQuickFind : public UnionFind{
+    friend std::ostream &operator<<(std::ostream &, const UnionFindQuickFind &);
+public:
+    explicit UnionFindQuickFind(int size):
+        id(new std::vector<int>(size)) {
+        for (int i = 0; i < size; i++)
+            (*id)[i] = i;
+    }
+
+    int getSize() const override { return id -> size(); };
+    bool isConnected(int p, int q) override  // 查看元素p和q是否同属于一个集合，时间复杂度：O(1)
+        { return find(p) == find(q); }
+    void unionElements(int p, int q) override;  // 合并元素p和q所属的集合，时间复杂度：O(n)
+
+private:
+    std::vector<int> *id;
+
+    int find(int p);   // 查找元素p所对应的集合编号，时间复杂度：O(1)
+};
+
+std::ostream &operator<<(std::ostream &, const UnionFindQuickFind &);
+
+#endif //ALGORITHM_TEMPLATE_CPP_UNION_FIND_QUICK_FIND_H
