5b3f4ddffc9f07127a314b550aa59fcca5eb7b99
AntonLiu <haoyuliuse@gmail.com>
2023-10-05 14:59:42 +0800

    新增QuickUnion并查集size优化实现

---
+++ tree/union_find_quick_union_with_size_optimization.cpp
@@ -0,0 +1,61 @@
+#include "union_find_quick_union_with_size_optimization.h"
+
+void UnionFindQuickUnionWithSizeOptimization::unionElements(int p, int q) {
+    int pRoot = find(p);
+    int qRoot = find(q);
+
+    if (pRoot == qRoot)
+        return;
+
+    if ((*sz)[pRoot] < (*sz)[qRoot]) {
+        (*parent)[pRoot] = qRoot;
+        (*sz)[qRoot] += (*sz)[pRoot];
+    }
+    else {  // (*sz)[pRoot] >= (*sz)[qRoot]
+        (*parent)[qRoot] = pRoot;
+        (*sz)[pRoot] += (*sz)[qRoot];
+    }
+}
+
+int UnionFindQuickUnionWithSizeOptimization::find(int p) {
+    if (p < 0 || p >= parent -> size())
+        throw std::runtime_error("索引非法！");
+
+    while (p != (*parent)[p])
+        p = (*parent)[p];
+
+    return p;
+}
+
+std::ostream &operator<<(std::ostream &os, const UnionFindQuickUnionWithSizeOptimization &uf) {
+    // 自适应边框
+    int size = uf.getSize();
+    os << "-----------------";
+    for (int i = 0; i < size; i++)
+        os << "---";
+    os << '\n';
+
+    // 数组信息
+    os << "并查集大小：" << size << "\n"
+       << "并查集数组表示：\n" << " index[";
+    for (int i = 0 ; i < size; i++) {
+        os << i;
+        if (i != size - 1)
+            os << ", ";
+    }
+    os << "]\n";
+
+    os << "parent[";
+    for (int i = 0 ; i < size; i++) {
+        os << (*uf.parent)[i];
+        if (i != size - 1)
+            os << ", ";
+    }
+    os << "]\n";
+
+    // 自适应边框
+    os << "-----------------";
+    for (int i = 0; i < size; i++)
+        os << "---";
+    return os;
+}
---
+++ tree/union_find_quick_union_with_size_optimization.h
@@ -0,0 +1,45 @@
+#ifndef ALGORITHM_TEMPLATE_CPP_UNION_FIND_QUICK_UNION_WITH_SIZE_OPTIMIZATION_H
+#define ALGORITHM_TEMPLATE_CPP_UNION_FIND_QUICK_UNION_WITH_SIZE_OPTIMIZATION_H
+
+#include "union_find.h"
+#include <vector>
+#include <iostream>
+
+/**
+ * 并查集Quick Union，集合大小优化
+ * 特点：
+ *      通过集合大小对合并进行优化
+ *      合并性能相比基本Quick Union有进一步提升
+ * 各操作时间复杂度：
+ *      isConnected: O(h)
+ *      find: O(h)
+ *      unionElements: O(h)
+ *      h是树的高度
+ */
+
+class UnionFindQuickUnionWithSizeOptimization : public UnionFind {
+    friend std::ostream &operator<<(std::ostream &, const UnionFindQuickUnionWithSizeOptimization &);
+public:
+    explicit UnionFindQuickUnionWithSizeOptimization(int size):
+            parent(new std::vector<int>(size)), sz(new std::vector<int>(size)) {
+        for (int i = 0; i < size; i++) {
+            (*parent)[i] = i;
+            (*sz)[i] = 1;
+        }
+    }
+
+    int getSize() const override { return parent -> size(); };
+    bool isConnected(int p, int q) override  // 查看元素p和q是否同属于一个集合
+    { return find(p) == find(q); }
+    void unionElements(int p, int q) override;  // 合并元素p和q所属的集合
+
+private:
+    std::vector<int> *parent;
+    std::vector<int> *sz;  // sz[i]表示以i为根的集合中元素的个数
+
+    int find(int p);   // 查找元素p所对应的集合编号
+};
+
+std::ostream &operator<<(std::ostream &, const UnionFindQuickUnionWithSizeOptimization &);
+
+#endif //ALGORITHM_TEMPLATE_CPP_UNION_FIND_QUICK_UNION_WITH_SIZE_OPTIMIZATION_H
